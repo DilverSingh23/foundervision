bcd10a6a8adfd3ae456cbc099b40a2b3f4164e83
DilverSingh23 <dsinghh23@gmail.com>
2025-06-05 15:50:47 -0400

    fetch api data and manage generating & error state

--- components/chatdisplay.js
+++ components/chatdisplay.js
@@ -2,17 +2,22 @@ import React, { useEffect, useState, useRef } from "react";
 import UserMessage from "./usermessage";
 import AiMessage from "./aimessage";
 import Typing from "./typing";
+import BusinessCanvas from "./businesscanvas";
 
-const chatdisplay = ({ userInput  }) => {
+const chatdisplay = ({ userInput }) => {
     const [show, setShow] = useState(false);
+    const [isGenerating, setIsGenerating] = useState(false);
+    const [canvasData, setCanvasData] = useState(null);
+    const [error, setError] = useState("");
     const startMessage = "Hey, I'm FounderAI🚀! Input your business idea in as much detail as possible and I will help you generate it into reality!";
-    const endMessage = "Got it! I'm generating a business canvas based on your idea🎨."
-    const bottomOfChat = useRef(null)
+    const generatingMessage = "Got it! I'm generating a business canvas based on your idea🎨.";
+    const bottomOfChat = useRef(null);
 
     useEffect(() => {
         if (userInput.length > 0) {
             setTimeout(() => {
                 setShow(true)
+                generateCanvas()
             }, 1500)
             clearTimeout()
         }
@@ -24,6 +29,33 @@ const chatdisplay = ({ userInput  }) => {
         }
     }, [show])
 
+    const generateCanvas = async () => {
+        setIsGenerating(true)
+
+        try {
+            const response = await fetch('/api/generate-canvas', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({businessIdea: userInput})
+            })
+
+            const result = await response.json()
+
+            if (response.ok) {
+                setCanvasData(result.data)
+            }
+            else {
+                setError("Error generating canvas: ", result.error)
+            }
+        } catch (err) {
+            setError("Error: ", err)
+        } finally {
+            setIsGenerating(false);
+        }
+    }
+
     return (
         <div className="flex flex-col w-200 h-11/12 gap-10 self-center overflow-auto mt-5 scroll-behavior-smooth">
             <AiMessage output = {startMessage} />
@@ -31,12 +63,20 @@ const chatdisplay = ({ userInput  }) => {
                 <>
                     <UserMessage message={userInput} />
                     {show && (
-                        <AiMessage output={endMessage} className="opacity-0 transition-opacity duration-1000" />
+                        <AiMessage output={generatingMessage} className="opacity-0 transition-opacity duration-1000" />
                     )}
-                    <div className="flex items-center">
-                        <img src="/founderai.png" className="w-25 h-25 self-center"/>
-                        <Typing className="" />
-                    </div>
+
+                    {isGenerating && (
+                        <div className="flex items-center">
+                            <img src="/founderai.png" className="w-25 h-25 self-center"/>
+                            <Typing />
+                        </div>
+                    )}
+
+                    {canvasData && (
+                        
+                    )}
+
                 </>
             )}
             <div ref={bottomOfChat} />
