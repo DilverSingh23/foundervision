c8a753e5dff13467a6d1f4f1a8f72816c7b6d278
DilverSingh23 <dsinghh23@gmail.com>
2025-06-20 16:47:37 -0400

    adjust guide for smaller devices

--- components/guide.js
+++ components/guide.js
@@ -9,32 +9,32 @@ const guide = () => {
     return (
         <div className="flex flex-col font-inter justify-center items-center mt-20">
             <h1 className={`bg-gradient-to-r bg-clip-text text-transparent from-rose-500 to-red-600 text-4xl font-extrabold transition-all duration-1000 ${titleInView ? "opacity-100" : "opacity-0"}`} ref={title}>Guide</h1>
-            <div className="flex flex-row gap-10 justify-center items-center mt-15 w-fit">
+            <div className="flex flex-row max-[1350px]:flex-col gap-10 justify-center items-center mt-15 w-fit">
                 <div className={`flex flex-col gap-5 border-l-3 pl-3 border-red-500 transition-all duration-800 ${guide1InView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"}`} ref={guide1}>
-                    <h1 className="text-white text-4xl font-extrabold">Enter Your Startup Idea</h1>
-                    <p className="text-gray-300 text-lg font-normal w-150">For the best results, describe your business idea clearly in 1-2 sentences. 
+                    <h1 className="text-white text-4xl max-[730px]:text-2xl font-extrabold">Enter Your Startup Idea</h1>
+                    <p className="text-gray-300 text-lg font-normal w-150 max-[730px]:w-100 max-[530px]:w-80">For the best results, describe your business idea clearly in 1-2 sentences. 
                     Include the product or service, target audience, and what makes it unique or valuable — the more specific and practical your idea, the more tailored and insightful the 
                     AI-generated canvas will be.
                     </p>
                 </div>
-                <img src="guide-image1.png" alt="Example Prompt" className={`self-center justify-self-center w-170 rounded-4xl transition-all duration-800  ${guide1InView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"}`}/>
+                <img src="guide-image1.png" alt="Example Prompt" className={`self-center justify-self-center w-170 max-[730px]:w-125 max-[530px]:w-90 rounded-4xl transition-all duration-800  ${guide1InView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"}`}/>
             </div>
-            <div className="flex flex-row gap-10 justify-center items-center mt-40 w-fit">
-                <img src="guide-image2.png" alt="View Canvas Image" className={`self-center justify-self-center w-170 rounded-4xl transition-all duration-800  ${guide2InView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"}`}/>
-                <div className={`flex flex-col gap-5 border-r-3 pr-3 border-red-500 transition-all duration-800  ${guide2InView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"}`}ref={guide2}>
-                    <h1 className="text-white text-4xl font-extrabold">View Your Business Canvas</h1>
-                    <p className="text-gray-300 text-lg font-normal w-150">Once FounderAI generates your canvas, your chat will be saved to the dashboard. You can view or download your business model by clicking the 'View/Download' button.
+            <div className="flex flex-row max-[1350px]:flex-col-reverse gap-10 justify-center items-center mt-40 w-fit">
+                <img src="guide-image2.png" alt="View Canvas Image" className={`self-center justify-self-center w-170 max-[730px]:w-125 max-[530px]:w-90 rounded-4xl transition-all duration-800  ${guide2InView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"}`}/>
+                <div className={`flex flex-col gap-5 border-r-3 max-[1350px]:border-r-0 max-[1350px]:border-l-3 pr-3 max-[1350px]:pr-0 max-[1350px]:pl-3 border-red-500 transition-all duration-800  ${guide2InView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"}`}ref={guide2}>
+                    <h1 className="text-white text-4xl font-extrabold max-[730px]:text-2xl">View Your Business Canvas</h1>
+                    <p className="text-gray-300 text-lg font-normal w-150 max-[730px]:w-100 max-[530px]:w-80">Once FounderAI generates your canvas, your chat will be saved to the dashboard. You can view or download your business model by clicking the 'View/Download' button.
                     </p>
                 </div>
             </div>
-            <div className="flex flex-row gap-10 justify-center items-center mt-40 w-fit">
+            <div className="flex flex-row max-[1350px]:flex-col gap-10 justify-center items-center mt-40 w-fit">
                 <div className={`flex flex-col gap-5 border-l-3 pl-3 border-red-500 transition-all duration-800  ${guide3InView ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"}`}ref={guide3}>
-                    <h1 className="text-white text-4xl font-extrabold">Download Your Business Canvas</h1>
-                    <p className="text-gray-300 text-lg font-normal w-150">After viewing your canvas, scroll to the bottom and click the download button 
+                    <h1 className="text-white text-4xl font-extrabold max-[730px]:text-2xl max-[415px]:text-[22px]">Download Your Business Canvas</h1>
+                    <p className="text-gray-300 text-lg font-normal w-150 max-[730px]:w-100 max-[530px]:w-80">After viewing your canvas, scroll to the bottom and click the download button 
                     to export your business model as a PDF.
                     </p>
                 </div>
-                <img src="guide-image3.png" alt="Download Button Image" className={`self-center justify-self-center w-170 rounded-4xl transition-all duration-800  ${guide3InView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"}`}/>
+                <img src="guide-image3.png" alt="Download Button Image" className={`self-center justify-self-center w-170 max-[730px]:w-125 max-[530px]:w-90 rounded-4xl transition-all duration-800  ${guide3InView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"}`}/>
             </div>            
         </div>    
 )}
--- components/mainbutton.js
+++ components/mainbutton.js
@@ -3,7 +3,7 @@ import React from 'react'
 const mainbutton = ({name, onClick}) => {
     return (
         <button onClick={onClick} className='text-white font-inter font-bold rounded-3xl p-3 w-50 bg-gradient-to-r from-rose-500 to-red-700
-         hover:from-red-600 hover:to-red-800 transition-all delay-1000 cursor-pointer'>
+         hover:from-red-600 hover:to-red-800 transition-colors delay-50 cursor-pointer'>
             {name}
         </button>
     )
